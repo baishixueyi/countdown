4efa7166419e57fef9a8189d2d2f239a910a771d
思想的自行车 <1375518791@qq.com>
2020-09-05 10:51:31 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,23 +1,28 @@
 //@param remainTime 剩余时间秒数
-function setStartTime(remainTime){
-    let timer
-    if(remainTime>0){
-      let day=parseInt(remainTime/86400)
-      let hour=parseInt((remainTime-86400*day)/3600)
-      let min =parseInt((remainTime-86400*day-3600*hour)/60)
-      let Seconds=(remainTime-86400*day-3600*hour-60*min)
+//@param count 执行次数 默认为0
+//@param now 第一次调用函数的时间
+ function setStartTime(remainTime,count=0,now) {
+    if (remainTime > 0) {
+      let day = parseInt(remainTime / 86400)
+      let hour = parseInt((remainTime - 86400 * day) / 3600)
+      let min = parseInt((remainTime - 86400 * day - 3600 * hour) / 60)
+      let seconds = (remainTime - 86400 * day - 3600 * hour - 60 * min)
       let cont = ''
       if(day>0){
-       cont = `未开始预约 ${day}天${hour}时${min}分${Seconds}秒`
+        cont = `距预约开始 ${day}天${hour}时${min}分${seconds}秒`
       }else {
-        cont = `未开始预约 ${hour}时${min}分${Seconds}秒`
+        cont = `距预约开始 ${hour}时${min}分${seconds}秒`
       }
-        //渲染页面
-        //定时任务
-        timer = setTimeout(()=>setStartTime(remainTime-1),1000)
-    }else{
-      //取消定时任务
-      clearTimeout(timer)
+      let inteval = 1000
+      let offset = +new Date()-(now+count*1000)
+      inteval = inteval-offset
+      if(inteval<1){inteval=0}
+      count++
+      remainTime--
+      timer = setTimeout(() => setStartTime(remainTime,count,now), inteval)
+    } else {
+      //取消定时任务 定时任务结束
+        clearTimeout(timer)
     }
-}
+  }
 export default {setStartTime}
